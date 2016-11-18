3a48c627cfa9e8a9a09a70096103c66338d63f6a
coutvv <coutvv@gmail.com>
2016-11-18 15:54:31 +0300

    Запилил граф. библиотеки и отработка клика
    По хорошему бы надо создать отдельный репозиторий с графической
    библеотекой для игр и от него отпочковываться. Разрабатывать можно ведь
    и паралельно! чо нет то?

--- src/main/java/ru/coutvv/cybersport/training/Game.java
+++ src/main/java/ru/coutvv/cybersport/training/Game.java
@@ -1,5 +1,23 @@
 package ru.coutvv.cybersport.training;
 
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+
+
 public class Game {
 
+	public static void main(String[] args) {
+
+		Screen screen = new Screen();
+		long window = screen.init();
+		screen.setClick();
+		
+		while(!glfwWindowShouldClose(window)) {
+			glfwSwapBuffers(window);
+			screen.clean();
+			glfwPollEvents();
+//			scene.turnAround();
+		}
+	}
 }
